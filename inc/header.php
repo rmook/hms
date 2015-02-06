5887c7293a8a7b1b00c1a65f22676ced0115eb7b
rmook <richard.mook@gmail.com>
2015-02-06 14:49:23 -0500

    split index.html into include files

---
+++ inc/header.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+	<title>Highland Motorsport | Vespa Louisville</title>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<header class=>
+		<div class="header-div">
+			<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-4">
+					<a href="index.html"><img src="icons/Logo.jpg" class="logo"></a>
+				</div>
+				<div class="col-sm-8">
+					<ul class="nav nav-pills" role="tablist">
+				  <li class="active"><a href="index.html">Home</a></li>
+				  <li class="dropdown">
+				  	<a href="scooters.html">Scooters<span class="caret"></span>
+				  	</a>
+				  	<ul class="dropdown-menu" role="menu">
+				  	</ul>
+			  	  </li>
+				  <li><a href="parts.html">Parts</a></li>
+				  <li><a href="faq.html">FAQ</a></li>
+				  <li><a href="about.html">About Us</a></li>
+
+				</ul>
+				</div>
+			</div>
+		</div>
+	</header>
--- index.php
+++ index.php
@@ -1,1 +1,84 @@
-index
+<?php
+
+include('inc/header.php');
+
+?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="slideshow center margin1">
+				    <ul class="list-no">
+				        <li>
+				        	<div class="jumbotron">
+				        		<div class="row">
+				        			<div class="col-xs-8">
+				        				<div class="promo">
+				        				<h1>2014 Model Year Clearance</h1>
+				        					<p> $500 off all 2014 Model Year scooters - Excellent savings on the brands you trust!</p>
+				        					<a class="btn action" href="#featured">Browse clearance items</a>
+				        					<p><span class="fancy">Only till November 2nd</span></p>
+			        					</div>
+			        				</div>
+			        				<div class="col-xs-4">
+			        					<img src="banners/clearance.png">
+			        				</div>
+		        				</div>
+				        	</div>
+			        	</li>
+				        <li><img src="banners/946.jpg" width="auto" height="auto"  alt="Vespa 946" class="slide"></li>
+				        <li><img src="banners/primavera.jpg" width="auto" height="auto"  alt="Vespa Primavera" class="slide"></li>
+				        <li><img src="banners/sprint.jpg" width="auto" height="auto"  alt="Vespa Sprint" class="slide"></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+			<div class="store-info">
+				<div class="row">
+					<div class="col-sm-4">
+						<h2>Store Hours</h2>
+						<div class="hours">
+							<p><span>Monday</span> Closed</p>
+							<p><span>Tuesday</span> 1pm - 6pm</p>
+							<p><span>Wednesday</span> 1pm - 6pm</p>
+							<p><span>Thursday</span> 1pm - 6pm</p>
+							<p><span>Friday</span> 1pm - 6pm</p>
+							<p><span>Saturday</span> 10am - 4pm</p>
+							<p><span>Sunday</span> Closed</p>
+							<p><em>Extended hours available</em></p>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<h2>Highland Motorsport</h2>
+						<div class="blurb">
+						<p>Louisville's premier source for quality, affordable scooter brands! We carry Vespa, Piaggio, Genuine, SYM, Lance, Bintelli and more. Stop by our showroom to find out more about the scooter way of life -<em>Two Wheels. To Go. Too Fun!</em></p>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<h2>Contact Information</h2>
+						<div class="contact">
+							<p>
+							<a class="btn" href="tel:5024512150"  target="_blank"><span class="phone"></span>(502) 451-2150</a><br>
+							<a class="btn" href="http://bit.ly/1xErM3Z"  target="_blank"><span class="address"></span> 1125 Bardstown Road<br> &nbsp; &nbsp; &nbsp; &nbsp; Louisville KY 40207</a><br>
+							<a class= "btn" href="mailto:service@highlandmotorsport.com"  target="_blank"><span class="email"></span>service@highlandmotorsport.com</a><br>
+							<a class="btn" href="http://www.facebook.com/highlandmotorsport" target="_blank"><span class="facebook"></span> Highland Motorsport</a>
+							</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		<div class="row">	
+			<div class="featured-scooters" id="featured">
+				<h1>Featured Bikes</h1>
+				<ul id="scooter-browse-list">
+				</ul>
+			</div>
+		</div>	
+	</div>
+
+<?php
+
+include('inc/footer.php');
+
+?>
